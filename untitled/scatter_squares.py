d10dbf8701e340e18e513ce1f63cb5aa4ef42a67
zsj <476241259@qq.com>
2018-12-03 23:00:49 +0800

    渐变色处理

--- untitled/scatter_squares.py
+++ untitled/scatter_squares.py
@@ -1,10 +1,9 @@
 import matplotlib.pyplot as plt
 
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
-
-plt.scatter(x_values, y_values, s=100)
+x_values = list(range(1001))
+y_values = [x**2 for x in x_values]
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolor='none', s=40)
 
 # 设置图表标题并给坐标轴指定标签
 plt.title("Square Numbers", fontsize=24)
